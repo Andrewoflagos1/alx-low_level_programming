1e875c38db4742ea670f63766d150749bb998220
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-06-15 21:20:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to a standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" -Doro korpar, 2015-10-19\n, stdou);
+return (1);
+}
