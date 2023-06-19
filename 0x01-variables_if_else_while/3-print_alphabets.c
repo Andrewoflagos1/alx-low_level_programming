562d15a559ebc3aebb07e714d79650df95059495
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-06-19 00:11:21 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabeth in lowercase, and then in uppercase.
- *
- * Return: Always 0.
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+	int n, m, l;
 
-	for (letter = 'a' letter <= 'z'; letter++)
-		putchar(letter);
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 	putchar('\n');
-
 	return (0);
-}
+}}
