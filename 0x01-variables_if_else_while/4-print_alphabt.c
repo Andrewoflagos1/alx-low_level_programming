63ebc9e313e1e4d80767374e317d910242d73a3e
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-06-19 08:07:34 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
-	int i, j;
+	char ch = 'a';
 
-	for (i = 0; i < 100; i++)
+	while (ch <= 'z')
 	{
-		for (j = 0; j < 100; j++)
+		if (ch != 'e' && ch != 'q')
 		{
-			if (i < j)
-			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
+			putchar(ch);
 		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
