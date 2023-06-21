4e1448420fb8c939661ef7a19fae560bf9069694
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-06-20 23:08:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print aline using the printf function
- * Return: 0 (success)
- *\
- int main(void)
- {
-      printf("with proper grammar, but the outcome is a piece of art,\n")
-      return (0);
+ * main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
