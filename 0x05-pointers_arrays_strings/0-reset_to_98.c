e4269ed29429f15b58da38ca97d70fbaada616d6
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-06-27 16:10:03 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,3 @@
-0-reset_to_98.c
-
 #include "main.h"
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
@@ -7,7 +5,9 @@
  * @n: input
  * Return: n
  */
-void reset_to_98(int *n)
+void reset_to_98(int *n);
 {
-	*n = 98;
+
+	
+	*n  =  98;
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * _strlen - returns the legnth of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s);
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+	longi++;
+	s++;
+
+	}
+
+}
