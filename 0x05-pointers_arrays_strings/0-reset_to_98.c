02052531938f1f6640349c3d0837043bea3ad788
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-06-28 01:02:01 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,9 +5,7 @@
  * @n: input
  * Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-
-	
-	*n  =  98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
-/*
- * _strlen - returns the legnth of a string
+/**
+ * _strlen - returns the length of a string
  * @s: string
  * Return: length
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int longi = 0;
 
 	while (*s != '\0')
 	{
-	longi++;
-	s++;
-
+		longi++;
+		s++;
 	}
 
+	return (longi);
 }
