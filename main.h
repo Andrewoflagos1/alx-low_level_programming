458e9a74d3399ac2739964d8dca82607a1ab7c01
Andrewoflagos1 <andrewmalary0@gmail.com>
2023-07-03 08:22:57 -0700

    1-memcpy

--- main.h
+++ main.h
@@ -1,19 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio,h>
-#include <unistd.h>
-
-int _puchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-
-#endif
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
